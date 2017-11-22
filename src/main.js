5f9edc7f9324c83117aa99641ca8954a5919873e
stijn <stijn@stijnvermeeren.be>
2017-11-22 20:44:52 +0100

    add google analytics

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import Vue from 'vue'
 
 import KeenUI from 'keen-ui';
 import 'keen-ui/dist/keen-ui.css';
+import VueAnalytics from 'vue-analytics'
 
 import App from './App.vue'
 import store from './store';
 
 Vue.use(KeenUI);
 
+Vue.use(VueAnalytics, {
+  id: 'UA-89547746-2'
+});
+
 new Vue({
   el: '#app',
   store,
   render: h => h(App)
-})
+});
