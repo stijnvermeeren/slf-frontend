0a536656c6e9aaeef46b56b042f3e534695ccda7
stijn <stijn@stijnvermeeren.be>
2018-02-17 20:34:15 +0100

    always show current SLF bulletin + layout tweaks

--- src/CategorySelect.vue
+++ src/CategorySelect.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        <div
-                v-for="category in categories"
-                :class="['category', {selected: category.value === value}]"
-                @click="$emit('input', category.value)"
-        >
-            <img :src="imageUrl(category.value)" :alt="category.label" />
-            <div class="label">{{category.label}}</div>
+        <div class="current">
+            <div class="label">
+                <a href="https://www.slf.ch/de/lawinenbulletin-und-schneesituation.html#schneedeckewetter">
+                    Current avalanche bulletin on slf.ch
+                </a>
+            </div>
+            <a href="https://www.slf.ch/de/lawinenbulletin-und-schneesituation.html#lawinengefahr">
+                <img src="http://www.slf.ch/avalanche/bki/bki_de_c_teaser.gif" alt="Current avalanche forecast from slf.ch" />
+            </a>
+        </div>
+        <div class="date">{{dateFullFormat}}</div>
+        <div class="categories">
+            <div
+                    v-for="category in categories"
+                    :class="['category', {selected: category.value === value}]"
+                    @click="$emit('input', category.value)"
+            >
+                <img :src="imageUrl(category.value)" :alt="category.label" />
+                <div class="label">{{category.label}}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -17,6 +30,9 @@
   export default {
     props: ['value', 'date', 'year'],
     computed: {
+      dateFullFormat() {
+        return dates.fullFormat(this.date);
+      },
       categories() {
         return [
           {
@@ -55,25 +71,59 @@
 </script>
 
 <style lang="scss" scoped>
-    div.category {
-        text-align: center;
-        margin-bottom: 12px;
+    div.current {
+        margin-top: 6px;
+        margin-bottom: 36px;
+        padding-top: 8px;
 
-        img {
-            width: 120px;
-        }
+        background-color: rgba(33, 150, 243, .3);
+        border-radius: 5px;
+
+        text-align: center;
+
         div.label {
+            font-weight: bold;
             font-size: 70%;
         }
-
-        &.selected {
-            div.label {
-                font-weight: bold;
-            }
+        img {
+            width: 160px;
         }
+    }
 
-        &:not(.selected) {
-            cursor: pointer;
+    div.date {
+        text-align: center;
+        font-size: 90%;
+        margin-bottom: 10px;
+        font-weight: bold;
+    }
+
+    div.categories {
+        display: grid;
+        grid-template-columns: 100px 100px;
+        grid-template-rows: 120px 120px 120px;
+
+        div.category {
+            text-align: center;
+            margin-bottom: 12px;
+
+            img {
+                width: 100px;
+            }
+            div.label {
+                font-size: 70%;
+            }
+
+            &.selected {
+                div.label {
+                    background-color: rgba(33, 150, 243, .3);
+                    border-radius: 5px;
+                    font-weight: bold;
+                }
+            }
+
+            &:not(.selected) {
+                cursor: pointer;
+            }
         }
     }
 </style>
